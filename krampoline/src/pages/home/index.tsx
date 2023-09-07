c859ed9b5f0d94a0a98cd260ba03fa3b3535b595
sumi-0011 <selina2000@naver.com>
2023-09-07 15:41:06 +0900

    feat: link login url

--- krampoline/src/pages/home/index.tsx
+++ krampoline/src/pages/home/index.tsx
@@ -1,13 +1,18 @@
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
 import TypingString from '../../components/TypingString';
 import { defaultFadeInUpVariants } from '../../constants/motions';
+
+const staticServerUrl = process.env.REACT_APP_PATH || '';
 
 function HomePage() {
   return (
     <div>
       <div>
         <TypingString>환영합니다!</TypingString>
+
+        <Link to={staticServerUrl + 'login'}>로그인 페이지로 이동</Link>
       </div>
     </div>
   );
